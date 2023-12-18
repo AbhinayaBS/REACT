7c3619202ce8bd32ea9c80383dcfae682c9a4402
AbhinayaBS <727822tuit004@skct.edu.in>
2023-12-18 14:41:38 +0530

    Day 9

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 // import Login from "./Component/Digix/Login"
 //import Signin from "./Component/Digix/Signinimport
 
-import ComponentLifeCycle from "./Component/Day8/ComponentLifeCycle";
+//import ComponentLifeCycle from "./Component/Day8/ComponentLifeCycle";
 //import TryCatch from "./Component/Day8/TryCatch";
-
 //import { ErrorBoundry } from "./Component/Day8/Car";
 //import ErrorBoundry1 from "./Component/Day8/ErrorBoundry1";
 
@@ -12,9 +11,9 @@ import ComponentLifeCycle from "./Component/Day8/ComponentLifeCycle";
 //import List from "./Component/Day6/List"
 //import List2 from "./Component/Day6/List2"
 
-export default function App(){
-    return(
-        <div>
+ //export default function App(){
+    // return(
+        // <div>
            {/* <ErrorBoundry> 
             <Hero heroName="Abhinaya"></Hero>
            </ErrorBoundry>
@@ -24,9 +23,39 @@ export default function App(){
            */}
            {/* <TryCatch fruit="Onion"></TryCatch> */}
 
-           <ComponentLifeCycle></ComponentLifeCycle>
+        //    <ComponentLifeCycle></ComponentLifeCycle>
 
 
-        </div>
-    )
+        // </div>
+    // )
+// }
+
+import logo from './logo.svg';
+import './App.css';
+import Main from "./components/Context/Main";
+import Purity from "./components/Purity";
+import StateHook from "./components/StateHook";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+  return <Main />;
 }
+
+export default App;
